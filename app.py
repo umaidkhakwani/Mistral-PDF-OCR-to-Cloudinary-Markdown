a4df065ab24dbadcc8b47a71cd7e8e36b1071175
umaidkhakwani <umaidkhakwani92@gmail.com>
2025-09-16 18:35:01 +0200

    Mistal OCR setup added

--- app.py
+++ app.py
@@ -26,7 +26,35 @@ cloudinary.config(
     secure=True
 )
 
+# -------- Mistral OCR Setup --------
+api_key = MISTRAL_API_KEY
+client = Mistral(api_key=api_key)
 
+pdf_file = Path("docs/physics1_removed.pdf")
+assert pdf_file.is_file(), "PDF file not found!"
+
+uploaded_file = client.files.upload(
+    file={
+        "file_name": pdf_file.stem,
+        "content": pdf_file.read_bytes(),
+    },
+    purpose="ocr",
+)
+
+signed_url = client.files.get_signed_url(file_id=uploaded_file.id, expiry=1)
+
+pdf_response = client.ocr.process(
+    document=DocumentURLChunk(document_url=signed_url.url),
+    model="mistral-ocr-latest",
+    include_image_base64=True
+)
+response_dict = json.loads(pdf_response.model_dump_json())
+
+
+# -------- Generate and Save Markdown --------
+combined_markdown = get_combined_markdown(pdf_response)
+
+print(combined_markdown)
 
 with open("output.md", "w") as f:
     f.write(combined_markdown)
